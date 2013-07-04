7087e28f709bc29de9a35eb30dfaf2755e102214
hedrick@rutgers.edu <hedrick@rutgers.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-07-04 17:23:53 +0000

    LSNBLDR-249; fix database conversion scripts
    git-svn-id: https://source.sakaiproject.org/svn/lessonbuilder/branches/lessonbuilder-1.4.x@126668 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- components/src/ddl/mysql/simplepage.for-2.9.3.sql
+++ components/src/ddl/mysql/simplepage.for-2.9.3.sql
@@ -1,6 +1,7 @@
 -- this should be harmless if it's already been done
 -- it applies to versions before sakai 2.9.3
 
+alter table lesson_builder_groups add siteId varchar(250);
 alter table lesson_builder_items modify description text;
 alter table lesson_builder_items modify groups text;
 alter table lesson_builder_items modify ownerGroups text;
--- components/src/ddl/oracle/simplepage.for-2.9.3.sql
+++ components/src/ddl/oracle/simplepage.for-2.9.3.sql
@@ -1,6 +1,8 @@
 -- this should be harmless if it's already been done
 -- it applies to versions before sakai 2.9.3
 
+alter table lesson_builder_groups add siteId varchar2(250 char);
+
 --- alter table lesson_builder_items modify description clob;
 alter table lesson_builder_items add temp clob;
 update lesson_builder_items set temp=description;
