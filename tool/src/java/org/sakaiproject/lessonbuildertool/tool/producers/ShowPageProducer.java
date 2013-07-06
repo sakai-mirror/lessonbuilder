50bb950897525d041cabff0e3f7571fa64c7a6b1
hedrick@rutgers.edu <hedrick@rutgers.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-07-06 19:24:10 +0000

    LSNBLDR-177; export question bank
    git-svn-id: https://source.sakaiproject.org/svn/lessonbuilder/branches/lessonbuilder-1.4.x@126709 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ShowPageProducer.java
+++ tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ShowPageProducer.java
@@ -2870,6 +2870,7 @@ public class ShowPageProducer implements ViewComponentProducer, DefaultView, Nav
 		UIForm form = UIForm.make(tofill, "export-cc-form");
 
 		UIOutput.make(form, "export-cc-v11"); // value is handled by JS, so RSF doesn't need to treat it as input
+		UIOutput.make(form, "export-cc-bank"); // value is handled by JS, so RSF doesn't need to treat it as input
 		UICommand.make(form, "export-cc-submit", messageLocator.getMessage("simplepage.exportcc-download"), "#{simplePageBean.importCc}");
 		UICommand.make(form, "export-cc-cancel", messageLocator.getMessage("simplepage.cancel"), null);
 
@@ -2877,6 +2878,7 @@ public class ShowPageProducer implements ViewComponentProducer, DefaultView, Nav
 		ExportCCViewParameters view = new ExportCCViewParameters("exportCc");
 		view.setExportcc(true);
 		view.setVersion("1.2");
+		view.setBank("1");
 		UIInternalLink.make(form, "export-cc-link", "export cc link", view);
 
 	}
